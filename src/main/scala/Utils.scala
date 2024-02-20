a796f1e4cb569b11cc81236e8c334d56f3866566
Clément <82080806+clementcst@users.noreply.github.com>
2024-02-20 15:37:57 +0100

    Update Utils.scala

--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -1,18 +1,31 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import SparkSQLApp.{sparkSession}
+import configSQLServer.{jdbcUrl, connectionProperties}
 
 object utils {
-    def printLine(): Unit = {
-         println("=" * 60);
-    }
+  def printLine(): Unit = {
+    println("=" * 60);
+  }
 
-    def printDataFrame(df:Option[DataFrame]): Unit = {
-        df match {
-            case None => println("None value was passed as a DataFrame in printDataFrame");
-            case _ => {
-                df.foreach(dataRow => dataRow.show());
-                printLine();
-            }
+  def printDataFrame(df: Option[DataFrame]): Unit = {
+    df match {
+      case None =>
+        println("None value was passed as a DataFrame in printDataFrame");
+      case _ => {
+        df.foreach(dataRow => dataRow.show());
+        printLine();
+      }
+    }
+  }
+
+    def loadJSON(filePath: String): Option[DataFrame] = {
+        try {
+            val jsonDF = sparkSession.read.option("mode", "DROPMALFORMED").json(filePath)
+            Some(jsonDF)
+        } catch {
+            case e: Exception =>
+            println(s"An error occurred while loading JSON file: ${e.getMessage}")
+            None
         }
     }
 }
-
